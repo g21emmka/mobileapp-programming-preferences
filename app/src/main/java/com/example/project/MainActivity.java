af33bc2c7c3862b1a26d2c1c87de1699eea51cc1
g21emmka <g21emmka@student.his.se>
2022-05-03 14:34:56 +0200

    Created an id for textview and created a java object

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.project;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewName;
+
+    SharedPreferences.Editor editor=
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textViewName = findViewById(R.id.name);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+        String name = preferences.getString("name", "inget namn hittat");
+        textViewName .setText(name);
+    }
 }
