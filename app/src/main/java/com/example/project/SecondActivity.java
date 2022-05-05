0d4586e0b1f8f368053cd013fed9e582bb3bcec9
g21emmka <g21emmka@student.his.se>
2022-05-05 16:34:55 +0200

    Added a finish activity in SecondActivity

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Button mainpage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +24,14 @@ public class SecondActivity extends AppCompatActivity {
     public void setName(View view) {
         EditText name = findViewById(R.id.name);
 
+        String getname = name.getText().toString();
+
         // Store the new preference
         SharedPreferences MyPreferences = this.getSharedPreferences("Preferences", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = MyPreferences.edit();
-        editor.putString("name", "Emma");
+        editor.putString("name", getname);
         editor.apply();
+
+        finish();
     }
 }
